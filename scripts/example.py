93f23be3ce648213eb66c4c7ea70a32663b3eaf2
Dustin Gooding <dustin.r.gooding@nasa.gov>
2015-09-17 14:17:11 -0500

    can't print the output of e.printBuffer()

--- scripts/example.py
+++ scripts/example.py
@@ -6,8 +6,10 @@ import ctypes
 
 buf = struct.pack('BBBB', 0x41, 0x42, 0x43, 0x44)
 
-print 'python:', buf
+print 'python:'
+print buf
 
 e = example_py.ExampleClass()
 
-print 'c++:', e.printBuffer(ctypes.cast(ctypes.c_char_p(buf), ctypes.POINTER(ctypes.c_ubyte)), ctypes.c_uint(4))
+print 'c++:'
+e.printBuffer(ctypes.cast(ctypes.c_char_p(buf), ctypes.POINTER(ctypes.c_ubyte)), ctypes.c_uint(4))
